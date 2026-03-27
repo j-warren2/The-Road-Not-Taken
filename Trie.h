722d9155cfe70f060c65c24b11fdb745ce947043
j-warren2 <163889170+j-warren2@users.noreply.github.com>
2026-03-27 08:45:45 -0400

    Add Trie data structure implementation

---
+++ Trie.h
@@ -0,0 +1,41 @@
+#ifndef TRIE_H
+#define TRIE_H
+
+#include <iostream>
+#include <unordered_map>
+#include <string>
+#include <vector>
+using namespace std;
+
+struct TrieNode {
+    unordered_map<char, TrieNode*> nextChoices;
+    bool isCompleteUserPath;
+    int usersPassedThrough;
+    TrieNode() : isCompleteUserPath(false), usersPassedThrough(0) {}
+};
+
+struct PathNode {
+    string pathSoFar;
+    int usersPassedThrough;
+};
+
+class Trie {
+private:
+    TrieNode* storyRoot;
+    vector<string> allStoredPaths;
+
+    void traverseForVisual(TrieNode* currentNode, string pathSoFar, vector<PathNode>& result);
+
+public:
+    Trie();
+
+    void insertDecisionPath(const string& decisionPath);
+    bool decisionPathExists(const string& decisionPath);
+    int countSharedChoices(const string& pathA, const string& pathB);
+    void printPerChoiceStatistics(const string& currentUserPath);
+    void findMostAndLeastSimilarPaths(const string& currentUserPath);
+    void collectVisualData(vector<PathNode>& result);
+    int getTotalUsers();
+};
+
+#endif
