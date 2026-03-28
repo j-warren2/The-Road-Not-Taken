e3e324be0d5ce69d5a0500de89c466ab917d5c6b
Gayathri G. <144268274+Gayathri-Gunda@users.noreply.github.com>
2026-03-28 19:35:30 -0400

    Moved HashMap.h from HashMap branch to main branch
    I wasn't understanding the pull request and how to merge the branches, so I just copied and pasted the file.

---
+++ HashMap.h
@@ -0,0 +1,49 @@
+
+#ifndef PROJECT_2_HASHMAP_HASHMAP_H
+#define PROJECT_2_HASHMAP_HASHMAP_H
+
+#include <string>
+#include <vector>
+#include <list>
+#include <utility>
+
+class HashMap {
+    int tableSize;
+    int totalEntries;
+    std::vector<std::list<std::pair<std::string, int>>> table;
+    std::vector<std::string> allStoredPaths;
+    int hash(std::string& key);
+
+public:
+    // Constructor
+    HashMap(int size = 133000);
+
+    // Inserts a decision path onto the map
+    void insertDecisionPath(std::string& decisionPath);
+
+    // Checks if a path exists
+    bool decisionPathExists(const std::string& decisionPath);
+
+    // Gets the number of users who took a certain path
+    int getPathCount(const std::string& decisionPath);
+
+    // Removes a path entirely
+    void removeDecisionPath(const std::string& decisionPath);
+
+    // Compares paths and scores similarities
+    int countSharedChoices(const std::string& pathA, const std::string& pathB);
+
+    // Compares how many stored users made the same choice at each step.
+    void printPerChoiceStatistics(const std::string& currentUserPath);
+
+    // Finds most and least similar paths
+    void findMostAndLeastSimilarPaths(const std::string& currentUserPath);
+
+    // Puts all paths into a vector
+    void collectVisualData(std::vector<std::pair<std::string, int>>& ans);
+
+    // Returns the total amount of users
+    int  getTotalUsers();
+};
+
+#endif //PROJECT_2_HASHMAP_HASHMAP_H
